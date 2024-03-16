2efdae3a6d03cf84f5d0f5a2b860a1f548b0b10d
Guillermo Masini <gsmasini@gmail.com>
2024-03-16 19:28:47 -0300

    Respaldo

--- script.js
+++ script.js
@@ -1,3 +1,144 @@
+$(document).ready(function() {
+
+    $('form').on('submit', function(event) {
+        event.preventDefault();
+    });
+
+    let projects = [];
+
+    const currentDate = new Date();
+    const dateString = currentDate.toISOString().split('T')[0];
+
+    $('#startDate, #endDate').val(dateString).attr('min', dateString);
+    $('#edit__form__date').val(dateString).attr('min', dateString);
+    
+    //Cerrar edit form
+    $('#edit__form__close').on('click', () => {
+        hideEditForm();
+    });
+
+    function hideEditForm() {
+        $('body').append($('#edit__form'));
+        $('#edit__form').css('visibility', 'hidden');
+        $('#edit__form__text').val("");
+        $('#edit__form__date').val(dateString).attr('min', dateString);
+    };
+
+    function projectCreate() {
+        const projectName = $('#projectName').val();
+        const startDate = $('#startDate').val();
+        const endDate = $('#endDate').val();
+
+        if (projectName === '') {
+            console.log('Intento fallido');
+            return;
+        }
+
+        const project = {
+            name: projectName,
+            creationDate: Date(),
+            startDate: startDate,
+            endDate: endDate
+        };
+
+        projects.push(project);
+        renderProjects();
+
+        $('#startDate, #endDate').val(dateString).attr('min', dateString);
+        $('#projectName').val("");
+    }
+
+    $('#createProject').click(function() {
+        projectCreate();
+    });
+
+    function renderProjects() {
+        //Avoid getting edit form deleted
+        hideEditForm();
+
+        $('#projectContainer').html('');
+        $.each(projects, function(index, project) {
+            const projectElement = $('<div></div>').addClass('__project__');
+            projectElement.html(`
+                <div id="proj__${index}">
+                    <span class="__project__modify">
+                        <button onclick='activatePopUpForm(${index},"rename")'
+                        class ="edit--button">Renombrar</button>
+
+                        <button onclick='activatePopUpForm(${index},"change-start-date")'
+                        class ="edit--button">Cambiar fecha inicio</button>
+
+                        <button onclick='activatePopUpForm(${index},"change-end-date")'
+                        class ="edit--button">Cambiar fecha límite</button>
+                    </span>
+                </div>
+                <h3>${project.name}</h3> 
+                <p>Fecha de inicio: ${project.startDate}</p>
+                <p>Fecha de finalización: ${project.endDate}</p>
+            `);
+            $('#projectContainer').append(projectElement);
+        });
+    }
+
+    window.activatePopUpForm = function(index, _type) {
+        const elem = $(`#proj__${index}`);
+        elem.append($('#edit__form'));
+        $('#edit__form').css('visibility', 'visible');
+        $('#edit__form__number').text(index);
+
+        switch(_type) {
+            case "rename":
+            $('#edit__form__type').text('RENAME');
+            $('#edit__form__text').css('display', 'initial');
+            $('#edit__form__date').css('display', 'none');
+            break;
+
+            case "change-start-date":
+            $('#edit__form__type').text('START');
+            $('#edit__form__date').css('display', 'initial');
+            $('#edit__form__text').css('display', 'none');
+            break;
+
+            case "change-end-date":
+            $('#edit__form__type').text('END');
+            $('#edit__form__date').css('display', 'initial');
+            $('#edit__form__text').css('display', 'none');
+            break;
+        }
+    };
+
+    function submitEditForm() {
+        var _type = $("#edit__form__type").text();
+        var index = Number($("#edit__form__number").text());;
+
+        switch(_type) {
+            case "RENAME":
+            projects[index].name = $('#edit__form__text').val();
+            break;
+            case "START":
+            projects[index].startDate = $('#edit__form__date').val();
+            break;
+            case "END":
+            projects[index].endDate = $('#edit__form__date').val();
+            break;
+        }
+        renderProjects();
+    };
+    //Subit edit form
+    $('#edit__form__submit').on('click', () => {
+        submitEditForm();
+    });
+});
+
+
+
+
+
+
+
+
+
+/*
 const createButton = document.getElementById('createProject');
 const currentDate = new Date();
 const dateString = currentDate.toISOString().split('T')[0];
@@ -90,3 +231,4 @@ function activatePopUpForm(index,_type) {
         break;
     }
 };
+*/
